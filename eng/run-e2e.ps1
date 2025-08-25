17e0b1ceb88b6822ed35f1a70fcf8f3ec6cebd91
jaltamirano <jose.alt@nearshoredevs.com>
2025-08-25 08:22:17 -0700

    Add console logger

--- eng/run-e2e.ps1
+++ eng/run-e2e.ps1
@@ -46,6 +46,7 @@ $e2eProj = "EwFrameworkAnalysis.Blazor.E2ETests\EwFrameworkAnalysis.Blazor.E2ETe
 Write-Host "`nRunning: dotnet test $e2eProj --configuration $Configuration"
 dotnet test $e2eProj --configuration $Configuration `
     --logger "trx;LogFileName=e2e.trx" `
+    --logger "console;verbosity=detailed" `
     || Fail "Playwright E2E tests failed."
 
 Pop-Location
