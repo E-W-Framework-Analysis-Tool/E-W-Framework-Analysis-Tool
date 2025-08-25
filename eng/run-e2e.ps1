5d638de17290850abcc173e06838b1d19064bbb3
jaltamirano <jose.alt@nearshoredevs.com>
2025-08-25 10:28:25 -0700

    Remove stack trace and error messages

--- eng/run-e2e.ps1
+++ eng/run-e2e.ps1
@@ -46,7 +46,7 @@ $e2eProj = "EwFrameworkAnalysis.Blazor.E2ETests\EwFrameworkAnalysis.Blazor.E2ETe
 Write-Host "`nRunning: dotnet test $e2eProj --configuration $Configuration"
 dotnet test $e2eProj --configuration $Configuration `
     --logger "trx;LogFileName=e2e.trx" `
-    --logger "console;verbosity=summary" `
+    --logger "console;verbosity=summary" 2>&1 | grep -vE "Stack Trace|at " `
     || Fail "Playwright E2E tests failed."
 
 Pop-Location
