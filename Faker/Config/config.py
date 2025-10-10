2d1e9f1e07095ed1255fe92c0d26d5e4c9bc9bae
Kirtana Nambiar <k.nambiar@wearedoubleline.com>
2025-10-10 16:52:51 -0400

    updated .gitignore file

--- Faker/Config/config.py
+++ Faker/Config/config.py
@@ -3,10 +3,8 @@ from os import environ
 
 def config():
     try:
-        server = 'DESKTOP-1R1DFJM\\SQLEXPRESS01'
-        database = 'CEDS-Data-Warehouse-V11-0-0-0'
-        # server = environ.get('SSMSServer')
-        # database = environ.get('FakerDatabase')
+        server = environ.get('SSMSServer')
+        database = environ.get('CEDS_DATABASE')
         return server, database
     except KeyError as e:
         print(f"Environment variable not set: {e}")
