f7ee668cf7013d456b186c7d117bae3b23d1b8c3
Michael Nichols <m.nichols@wearedoubleline.com>
2025-11-05 10:30:02 -0500

    Formatting

--- src/EwFrameworkAnalysis.UI/AppRoutes.cs
+++ src/EwFrameworkAnalysis.UI/AppRoutes.cs
@@ -16,13 +16,13 @@ public static class AppRoutes
     public static readonly Dictionary<string, string> RouteTitles = new()
     {
         public static string ForDataSource(Guid id) => $"/data-sources/{id}";
-        public static bool IsMatch(string path) => path.StartsWith("/data-sources/");
-        public static Guid? ExtractDataSourceId(string path)
-        {
-            var segments = path.Split('/');
-            if (segments.Length >= 3 && Guid.TryParse(segments[2], out var id))
-                return id;
-            return null;
-        }
+    public static bool IsMatch(string path) => path.StartsWith("/data-sources/");
+    public static Guid? ExtractDataSourceId(string path)
+    {
+        var segments = path.Split('/');
+        if (segments.Length >= 3 && Guid.TryParse(segments[2], out var id))
+            return id;
+        return null;
     }
+}
 };
