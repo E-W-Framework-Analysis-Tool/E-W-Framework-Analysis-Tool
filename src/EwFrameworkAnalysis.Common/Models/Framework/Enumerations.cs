1396e4ea4f93be5735dc107199ca581e4c9c9229
Jose Altamirano <73850589+JoseAltNSD@users.noreply.github.com>
2025-11-12 11:17:43 -0700

    Add Reports and Analysis page (#26)
    * Add Analysis page and components
    
    * Add Indicator Modal

--- src/EwFrameworkAnalysis.Common/Models/Framework/Enumerations.cs
+++ src/EwFrameworkAnalysis.Common/Models/Framework/Enumerations.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EwFrameworkAnalysis.Common.Models.Framework;
 
 public enum Sector
 {
+    [Display(Name = "Pre-K", Description = "")]
     PK,
+
+    [Display(Name = "k-12", Description = "")]
     K12,
+
+    [Display(Name = "Postsecondary", Description = "")]
     PS,
+
+    [Display(Name = "Workforce", Description = "")]
     WF
 }
