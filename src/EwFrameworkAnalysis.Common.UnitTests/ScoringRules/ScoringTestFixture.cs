35ae3b22e58fb3ccc2d242ed9b9082c98c5d1190
jaltamirano <jose.alt@nearshoredevs.com>
2026-02-18 12:21:01 -0700

    Modify ECS State data details page

--- src/EwFrameworkAnalysis.Common.UnitTests/ScoringRules/ScoringTestFixture.cs
+++ src/EwFrameworkAnalysis.Common.UnitTests/ScoringRules/ScoringTestFixture.cs
@@ -20,7 +20,7 @@ public class ScoringTestFixture : IDisposable
         };
 
         RuleRegistry = new DataElementScoringRuleRegistry(rules);
-        ScoringService = new DataElementScoringService(RuleRegistry, new EcsStateDataProvider());
+        ScoringService = new DataElementScoringService(RuleRegistry);
     }
 
     public void Dispose()
