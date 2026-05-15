0aaeac61a332b30bd9c33f930550ca9b0c6de9d3
Michael Nichols <m.nichols@wearedoubleline.com>
2026-05-15 13:58:18 -0400

    Add dev utilities used for framework updates

--- src/EwFrameworkAnalysis.Common/FrameworkReferenceData/Disaggregate.cs
+++ src/EwFrameworkAnalysis.Common/FrameworkReferenceData/Disaggregate.cs
@@ -4,8 +4,8 @@ namespace EwFrameworkAnalysis.Common.FrameworkReferenceData;
 
 public class Disaggregate
 {
-    public required string Name { get; init; }
-    public required string Description { get; init; }
-    public required List<Sector> Sectors { get; init; }
-    public List<string> DataElementNames { get; init; } = [];
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required List<Sector> Sectors { get; set; }
+    public List<string> DataElementNames { get; set; } = [];
 }
